ed43016d3e7fb4c44f2ae37dbb3298a7a1e069ba
SweetSpawt <69224016+SweetSpawt@users.noreply.github.com>
2020-09-07 12:53:25 -0500

    Update main.go

--- main.go
+++ main.go
@@ -10,14 +10,13 @@ import (
 func main() {
 	fmt.Print("Enter text: ")
 	reader := bufio.NewReader(os.Stdin)
-	// ReadString will block until the delimiter is entered
+	
 	input, err := reader.ReadString('\n')
 	if err != nil {
 		fmt.Println("An error occured while reading input. Please try again", err)
 		return
 	}
-
-	// remove the delimeter from the string
+	
 	input = strings.TrimSuffix(input, "\n")
 	fmt.Println(input)
 }
